706130eec2a0d9beaf9186fa57455c281106429d
tofu511 <mokmok511@gmail.com>
2019-01-03 02:28:51 +0900

    change machine type

--- terraform/gcp/variables.tf
+++ terraform/gcp/variables.tf
@@ -7,5 +7,5 @@ variable "additional_zones" {
   default = [ "asia-northeast1-b", "asia-northeast1-c", ]
 }
 variable "initial_node_count" { default = 1 }
-variable "machine_type" { default = "n1-standard-1" }
+variable "machine_type" { default = "n1-standard-4" }
 variable "disk_size_gb" { default = 10 }
