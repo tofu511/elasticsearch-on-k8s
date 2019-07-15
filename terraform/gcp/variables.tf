3b4117c4339f3102e006a241124cd3eb6013ee33
tofu511 <mokmok511@gmail.com>
2019-07-15 13:42:34 +0900

    update default initial node count

--- terraform/gcp/variables.tf
+++ terraform/gcp/variables.tf
@@ -6,6 +6,6 @@ variable "zone" { default = "asia-northeast1-a"}
 variable "additional_zones" {
   default = [ "asia-northeast1-b", "asia-northeast1-c", ]
 }
-variable "initial_node_count" { default = 2 }
+variable "initial_node_count" { default = 4 }
 variable "machine_type" { default = "n1-standard-2" }
 variable "disk_size_gb" { default = 10 }
